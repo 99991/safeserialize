8ffe32f4c497f0eb17b5ea093bc4e9832c24b54d
99991 <99991@users.noreply.github.com>
2025-08-27 12:09:07 +0200

    add readers and writers for pathlib in python 3.11

--- safeserialize/types/stdlib/pathlib.py
+++ safeserialize/types/stdlib/pathlib.py
@@ -9,17 +9,33 @@ We convert to "a/b" with as_posix().
 """
 
 @writer("pathlib._local.PosixPath")
-def write_posix_path(data, out):
+def write_local_posix_path(data, out):
     write_str(data.as_posix(), out)
 
 @reader("pathlib._local.PosixPath")
+def read_local_posix_path(f):
+    return Path(read_str(f))
+
+@writer("pathlib.PosixPath")
+def write_posix_path(data, out):
+    write_str(data.as_posix(), out)
+
+@reader("pathlib.PosixPath")
 def read_posix_path(f):
     return Path(read_str(f))
 
 @writer("pathlib._local.WindowsPath")
-def write_windows_path(data, out):
+def write_local_windows_path(data, out):
     write_str(data.as_posix(), out)
 
 @reader("pathlib._local.WindowsPath")
+def read_local_windows_path(f):
+    return Path(read_str(f))
+
+@writer("pathlib.WindowsPath")
+def write_windows_path(data, out):
+    write_str(data.as_posix(), out)
+
+@reader("pathlib.WindowsPath")
 def read_windows_path(f):
     return Path(read_str(f))
